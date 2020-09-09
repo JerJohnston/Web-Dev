64f3a8b74e455f41dd64f9c253abba12a5a73234
JerJohnston <59575963+JerJohnston@users.noreply.github.com>
2020-09-08 21:31:01 -0600

    finished basic three tutorial

--- 3JS Practice/Basic 3JS Scene/js/main.js
+++ 3JS Practice/Basic 3JS Scene/js/main.js
@@ -1,14 +1,14 @@
+function Log(arg){ console.log(arg); }
 
-var SphereGrid = function(){
+var LessonThreeJS = function(){
+   var me = this;
 
-    var me = this; 
-
-    var camera,
-        cameraController,
-        scene,
-        renderer;
-
-    var container,
+    var scene,
+        renderer,
+        camera,
+        cameraController;
+    
+    var container, 
         light,
         sphereGeom,
         sphereMaterial,
@@ -17,66 +17,65 @@ var SphereGrid = function(){
     function init(){
 
         container = document.createElement("div");
-        container.id = "MyThreeJsScene";
+        container.id = "MyThreeJSScene";
         document.body.appendChild(container);
-
-        scene = new THREE.scene();
-
-        renderer = new THREE.WebGLRenderer({ antialias: true });
-        renderer.setClearColor(0X000000);
+    
+        scene = new THREE.Scene();
+    
+    
+    
+        renderer = new THREE.WebGLRenderer({ antialias: true});
+        renderer.setClearColor(0x000000);
         renderer.setSize(window.innerWidth,window.innerHeight);
         container.appendChild(renderer.domElement);
-
-        camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 10000);
+    
+        camera = new THREE.PerspectiveCamera(45,window.innerWidth/window.innerHeight,0.1,10000);
         camera.position.set(0,100,300);
-
-        cameraController = new THREE.OrbitControls(camera, renderer.domElement);
+    
+        cameraController = new THREE.OrbitControls(camera,renderer.domElement);
         cameraController.target.set(0,0,0);
         cameraController.enableDamping = true;
-        cameraController.dampingFactor = 0.05;
-        cameraController.rotateSpeed = 0.045;
-
+        cameraController.dampingFactor = 0.025;
+        cameraController.rotateSpeed = 0.05;
+    
         
-        buildScene();
 
+        buildScene();
+    
         render();
+
     }
 
-    function buildScene(){
-        var gridHelper = new THREE.GridHelper(100,10);
+function buildScene(){
+    var gridHelper = new THREE.GridHelper(250,25);
         scene.add(gridHelper);
 
-        light = new THREE.DirectionalLight(0xFFFFFF, 1);
+    light = new THREE.DirectionalLight(0xFFFFFF,1.5);
+    scene.add(light);
 
-        sphereGeom = new THREE.SphereGeometry(10,4,4);
-        sphereMaterial = new Three.MeshPhongMaterial({color:0xCC0000});
-        sphereMaterial.specular = new Three.Color(255,255,255);
-        sphereMesh = new THREE.Mesh(sphereGeom, sphereMaterial);
-        scene.add(sphereMesh);
-    }
-     
-    function render(){
-        cameraController.update();
-        renderer.render(scene,camera);
+    sphereGeom = new THREE.SphereGeometry(25,25,25);
+    sphereMaterial = new THREE.MeshPhongMaterial({color:0xCC0000});
+    //sphereMaterial.wireframe = true;
+    var specColor = 0.1;
+    sphereMaterial.specular = new THREE.Color(specColor,specColor,specColor);
+    sphereMesh = new THREE.Mesh(sphereGeom,sphereMaterial);
+    sphereMesh.position.y = 40;
 
-        requestAnimationFrame(render);
-    }
-
-    init();
-    
-};
-
-
-
-
-
-
-
-
-function init(){
-    new SphereGrid();
-
+    scene.add(sphereMesh);
 }
 
+function render(){
+    cameraController.update();
+    renderer.render(scene,camera);
 
+    requestAnimationFrame(render);
+}
 
+init();
+
+   
+};
+    
+function init(){
+    new LessonThreeJS();
+};
