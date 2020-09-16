40801fdf470f94c003d7301984481438b4e5ec68
JerJohnston <59575963+JerJohnston@users.noreply.github.com>
2020-09-16 09:25:51 -0600

    more class updates

---
+++ CPSC1520/week-02/week-002/shipping-START-files/js/index.js
@@ -0,0 +1,29 @@
+// CTRL + ?
+
+/*
+
+ALT + SHIFT + A
+
+console.log()
+console.table()
+console.warn()
+console.error()
+console.clear()
+
+JavaScript is a .syntax language meaning that the dot seperates the keywords of the language
+
+
+
+Object . Method/property 
+*/
+
+const shippingWeight = document.querySelector('input');
+console.log(shippingWeight)
+
+
+const calcImage = document.querySelector('aside img:last-of-type');
+console.log(calcImage);
+
+const calcBtn = document.querySelector('label');
+console.log(calcBtn);
+
